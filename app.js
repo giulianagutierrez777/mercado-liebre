60df40e4b5777146dc4ab5d00f3383ef066493b9
giulianagutierrez777 <giulianagutierrez777@gmail.com>
2023-01-01 10:44:40 -0300

    consignas tp1y2

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 5500;
+
+app.listen(port, ()=> console.log(`servidor corriendo en http://localhost ${port}`));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
