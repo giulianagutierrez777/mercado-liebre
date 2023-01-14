d14e3961eb71430717049c3ab8b57b4abeee7dc7
giulianagutierrez777 <giulianagutierrez777@gmail.com>
2023-01-14 00:50:56 -0300

    finalizacion de los tps

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ app.listen(port, ()=> console.log(`servidor corriendo en http://localhost ${port
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'usuarios', 'login.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'usuarios', 'register.html'))); 
